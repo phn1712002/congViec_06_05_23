b505b5be36e8a89e24ef0b6e63e49faafbb5a265
HoangggNam <phn1712002@gmail.com>
2023-05-06 21:26:50 +0700

    Cập nhật nội dung hàm main và chia dữ liệu [main.m, splitData.m]

--- Neural Network/main.m
+++ Neural Network/main.m
@@ -3,9 +3,12 @@ close all;
 clc;
 clear;
 
-%% Tải dữ liệu cho mô hình 
+%% Xử lý dữ liệu
+% Tải dữ liệu cho mô hình 
 nameFile = 'dataModelRegression.xlsx';
 data = xlsread(nameFile);
 
+% Chia dữ liệu thành các tập training và test
+[dataTraining, dataTest] = splitData(data, 0.8);
 
 
--- Neural Network/splitData.m
+++ Neural Network/splitData.m
@@ -1,3 +1,13 @@
-function splitData(X, ratioTrain)
+function [Training, Test] = splitData(X, ratioTrain)
+    % Tính toán số lượng phần tử trong tập Training dựa vào phần trăm ratioTrain
+    lengthTraining = floor(length(X) * ratioTrain);
+
+    % Trộn ngẫu nhiên bộ dữ liệu
+    idx = randperm(size(X, 2));
+    X = X(:, idx);
+
+    % Chia dữ liệu về các tập
+    Training = X(:, 1:lengthTraining);
+    Test = X(:, lengthTraining + 1:end);
     
 end
