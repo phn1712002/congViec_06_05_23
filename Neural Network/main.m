cd800d14921e3767503293572843f9f7f9254b72
HoangggNam <phn1712002@gmail.com>
2023-05-07 16:26:57 +0700

    Cập nhật các tính năng cho main, splitData, .xlsx

--- Neural Network/main.m
+++ Neural Network/main.m
@@ -6,9 +6,42 @@ clear;
 %% Xử lý dữ liệu
 % Tải dữ liệu cho mô hình 
 nameFile = 'dataModelRegression.xlsx';
-data = xlsread(nameFile);
+data = readtable(nameFile);
 
 % Chia dữ liệu thành các tập training và test
 [dataTraining, dataTest] = splitData(data, 0.8);
 
+%% Khởi tạo mô hình
+% Cấu trúc mạng nơ rơn
+numFeatureInput = size(dataTraining, 2) - 1; 
+layers = [
+    % Layer Input
+    featureInputLayer(numFeatureInput, 'Name', 'input')
+    
+    % Hidden Layer 1
+    fullyConnectedLayer(128, 'Name', 'fc_1')
+    reluLayer('Name','rl_1')
+    % Hidden Layer 2
+    fullyConnectedLayer(128, 'Name', 'fc_2')
+    reluLayer('Name', 'rl_2')
+
+    % Layer Output
+    fullyConnectedLayer(1, 'Name', 'output')
+    regressionLayer('Name', 'regression')
+];
+% Khởi tạo cách thức training
+options = trainingOptions('adam');
+options.MaxEpochs = 1000;
+options.Verbose = false;
+options.MiniBatchSize = 16;
+% Khởi tạo cấu hình model
+layersGraph = layerGraph(layers);
+
+
+%% Huấn luyện model trên tập training
+model = trainNetwork(dataTraining, layersGraph, options);
+
+%% Kiểm tra trên tập test 
+yPred = predict(model, dataTest);
+pref = calcPerf(yPred, dataTest.Y);
 
--- Neural Network/splitData.m
+++ Neural Network/splitData.m
@@ -1,13 +1,13 @@
 function [Training, Test] = splitData(X, ratioTrain)
     % Tính toán số lượng phần tử trong tập Training dựa vào phần trăm ratioTrain
-    lengthTraining = floor(length(X) * ratioTrain);
+    lengthTraining = floor(size(X, 1) * ratioTrain);
 
     % Trộn ngẫu nhiên bộ dữ liệu
     idx = randperm(size(X, 2));
     X = X(:, idx);
 
     % Chia dữ liệu về các tập
-    Training = X(:, 1:lengthTraining);
-    Test = X(:, lengthTraining + 1:end);
+    Training = X(1:lengthTraining, :);
+    Test = X(lengthTraining + 1:end, :);
     
 end
